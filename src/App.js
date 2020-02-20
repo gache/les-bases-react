f35c3b597c1317855e04167782450556ffa42a07
Gache <erickfrancodelgado@hotmail.com>
2020-02-20 11:25:59 +0100

    Mon premier composant react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> salut tittre 1 </h1> 
+        <div>
+            <Header />
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { appendFile } from 'fs';
+
+function Header(){
+    return(
+        <h1>Salut Header</h1>
+        );
+}
+
+export default Header;
